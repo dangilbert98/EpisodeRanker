b91bae3fc77b70194d34500cdb681b1becc4ca69
Dan <dangilbert98@gmail.com>
2020-11-18 20:44:22 -0500

    fixed season count

--- src/main/java/com/dantheman/episode_ranker/models/User.java
+++ src/main/java/com/dantheman/episode_ranker/models/User.java
@@ -4,16 +4,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Episode {
-    private Integer id;
-    private Integer season_number;
-    private Integer episode_number;
-    private String name;
-    private String overview;
+public class User {
+    private String username;
+    private String password;
+    private List<EpisodeOrderList> episodeOrderLists;
 }
